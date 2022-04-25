c66f72a722d4d40289c07071fb30b605eccad357
minefarmer <pgoldfarm@yahoo.com>
2022-04-25 17:31:22 -0500

    moving to linux

--- classNotes/loops_iterations/looping.py
+++ classNotes/loops_iterations/looping.py
@@ -5,8 +5,8 @@ Write a program that can take the positive numbers from user. The loop should be
         Enter Numbers in valid range  Solution 1   # Not working
 Write a program that can take the numbers from 1 to 10. If user enters number in a valid range (1 to 10) then program should display the message "Congratulations, you entered the number in correct range". Otherwise, yhe program should ask the user to re-enter the number until user enters the correct number.    Line 25
 
-        Enter Numbers in valid range  Solution 2
-
+        Enter Numbers in valid range  Solution 2  Line 45
+        
 
 
 
@@ -22,7 +22,7 @@ Write a program that can take the numbers from 1 to 10. If user enters number in
                                                         # You entered  11
                                                         # Enter the value of num again: -2
    
-'''    Pseudocode (Method # 1)
+'''    Pseudocode (Method # 1)  # not working
 
 1. Start
 2. Enter the number
@@ -33,9 +33,34 @@ Write a program that can take the numbers from 1 to 10. If user enters number in
 
 '''
 
-num = int(input('Enter the value of num: '))
+# num = int(input('Enter the value of num: '))
 
-while N < 1 or N > 10:
-    N = int(input("Please enter the number in range 1 to 10: "))
-print("Congratulations, You entered the number in correct range")
+# while N < 1 or N > 10:
+#     N = int(input("Please enter the number in range 1 to 10: "))
+# print("Congratulations, You entered the number in correct range")
 
+
+
+
+'''     Pseudocode (Method # 2) 
+
+1. Start
+2. Enter the number
+3. Start an infinate loop
+        1. If number is less than 1 or greater than 10
+                1. Reenter the number3
+        2. Otherwise do the following
+                1. Display the message
+                2. Break/exit from the loop
+3. End
+
+'''
+
+N  = int(input("Please enter the number: "))
+
+while True:
+    if N < 1 or N > 10:
+        N = int(input("Please re-enter the number in range 1 to 10: "))
+else:
+    print("Congratulations, you entered the number in correct range")
+    break
