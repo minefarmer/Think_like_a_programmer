12f4e82bc02c7fff19104e0a9625c9501c5d9a6d
minefarmer <pgoldfarm@yahoo.com>
2022-05-01 00:47:54 -0500

    good night

---
+++ classNotes/data_structures/list_basics.py
@@ -0,0 +1,53 @@
+'''         Agenda
+
+Purpose of lists
+Creating lists
+Empty lists
+Adding things to a list
+Getting things from a list
+
+
+>>> mylist = [10, 20, 30, 40]
+>>> mylist
+[10, 20, 30, 40]
+>>> names = ["Tahir", "Rich", "Sis", "Roger"]
+>>> names
+['Tahir', 'Rich', 'Sis', 'Roger']
+>>> # lists can hold anything
+>>> mixture = ["tahir", 10, 20.98, 'male']
+>>> mixture
+['tahir', 10, 20.98, 'male']
+>>> # empty list
+>>> a = []
+>>> a
+[]
+>>> # using append() function
+>>> a = []
+>>> a.append(10)
+>>> a
+[10]
+>>> a
+[10, 20]
+>>> a.append(30)
+>>> a
+[10, 20, 30]
+>>> # getting elements from a list
+>>> a = [10, 20, 30]
+>>> a[0]
+10
+>>> # insert() method
+>>> a = [10, 20, 30, 40]
+>>> a.insert(1, 5)
+>>> a
+[10, 5, 20, 30, 40]
+>>> # extend method
+>>> a.extend([60, 70])
+>>> a
+[10, 5, 20, 30, 40, 60, 70]
+>>> x = [100, 200, 300]
+>>> a.extend(x)
+>>> a
+[10, 5, 20, 30, 40, 60, 70, 100, 200, 300]
+>>> 
+
+'''
