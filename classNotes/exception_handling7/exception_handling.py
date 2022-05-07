d4276ac3173c5bc60da02935779f5365fed833f2
minefarmer <pgoldfarm@yahoo.com>
2022-05-07 16:48:26 -0500

    cleaning up

---
+++ classNotes/exception_handling7/exception_handling.py
@@ -0,0 +1,13 @@
+'''         Exception Handling: Part 1
+            Exception Handling: Part 2
+
+    Common types of exceptions
+Division by zero
+Key error
+Value error
+Type error
+Many others...
+
+
+
+'''
